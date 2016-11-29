4648fbfb0ffb72afa3590137422f92a727e485f1
Jean-Paul Wilson <jeanpaulwilson@gmail.com>
2016-11-29 20:37:36 +0200

    Add basic signup form validation but not yet linked to database

--- main.py
+++ main.py
@@ -21,12 +21,15 @@ This blog must have:
 3. Permalink page for entries 
 '''
 import os 
+import re  #This is the regular expression library!
 import webapp2
 import jinja2
 import string
 import random
 from google.appengine.ext import db 
 
+
+# This is all helper stuff....
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
 
@@ -41,14 +44,38 @@ class Handler(webapp2.RequestHandler):
 	def render(self, *template, **params):
 		return self.write(self.render_str(*template, **params))
 
+#VALID INPUTS..
+
+def valid_username(username):
+		return username and USER_RE.match(username)
+#The line above means the same as:
+#	If there is a username, & it matches thes regex, then return True 
+
+def valid_password(password):
+		return password and PASS_RE.match(password)
+
+def valid_email(email):
+		return not email or EMAIL_RE.match(email)
+
+#REGEX:
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{5,20}$")
+PASS_RE = re.compile(r"^.{3,20}$")
+EMAIL_RE = re.compile(r"^[\S]+@[\S]+\.[\S]+$")
 
 
-#main page
+#########      ---- MAIN PAGE ----
+
 class MainPage(Handler):
 	def get(self):
 		posts = db.GqlQuery("SELECT * FROM BlogEntry ORDER BY timestamp DESC")
 
-		self.render("homepage.html", posts = posts)
+		username = self.request.get("username")
+		if valid_username(username):
+			self.render("homepage.html", username=username, posts = posts)
+
+		else:
+			self.render("homepage.html", posts = posts, username="Guest")
+
 
 #db entries:  
 def blog_key(name='dafault'):
@@ -64,6 +91,40 @@ class BlogEntry(db.Model):
 class SignUp(Handler):
 	def get(self):
 		self.render("signup.html")
+
+	def post(self):
+		have_error = False #If make it through the whole page with no errors, then render the success page
+		username = self.request.get("username")
+		password = self.request.get("password")
+		verify = self.request.get("verify")
+		email = self.request.get("email")
+
+		params = dict(username = username, email = email) #This is for string substitution back into the signup form
+
+		#Now, checking the signup form inputs: 
+		if not valid_username(username):
+			params["name_error"] = "That is not a valid username"
+			have_error = True
+
+		if not valid_password(password):
+			params["password_error"] = "That is not a valid password"
+			have_error = True
+
+		elif password != verify:
+			params["verify_error"] = "Your passwords do not match"
+			have_error = True
+
+		if not valid_email(email):
+			params["email_error"] = "That is not a valid email address"
+			have_error = True
+
+		if have_error:
+			self.render("signup.html", **params)
+		else:
+			self.redirect('/?username=' + username)
+
+
+
 
 #Blog post page
 class FormPage(Handler):
@@ -89,5 +150,5 @@ class FormPage(Handler):
 
 
 app = webapp2.WSGIApplication([('/', MainPage),
-								('/form', FormPage),
+								('/form', FormPage), #This is where you make a blog submission
 								('/signup', SignUp)], debug = True)
