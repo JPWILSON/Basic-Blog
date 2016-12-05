2afdad8888e78a83f45a50a4f4121b9ab5ed14ce
Jean-Paul Wilson <jeanpaulwilson@gmail.com>
2016-12-05 18:33:54 +0200

    updating comments

--- main.py
+++ main.py
@@ -205,39 +205,60 @@ class Comment(db.Model):
     commentid = db.IntegerProperty(required=True)
     created = db.DateTimeProperty(auto_now_add=True)
 
-
-class Comments(Handler):
-    """class that handles a new comment"""
+class CommentHandler(Handler):
     def get(self, post_id):
         key = db.Key.from_path('BlogEntry', int(post_id), parent = blog_key())
         p = db.get(key)
-        q = int(p.key().id())
-        c = db.GqlQuery("SELECT comment FROM Comment WHERE commentid = :q", q=q)
+        commentid = int(p.key().id())
+        all_comments = Comment.all().filter('commentid =', commentid).order('-created')
 
         if self.user:
-            self.render("comment.html", p=p, subject=p.subject, content=p.content, commentxist=c)
+            self.render("comment.html", p=p, all_comments = all_comments)
         else:
-            self.redirect('/')
+            error = "You need to be signed in to comment on a post"
+            redirect('/signup', error = error)
 
     def post(self, post_id):
         key = db.Key.from_path('BlogEntry', int(post_id), parent = blog_key())
         p = db.get(key)
-
-        commentin = self.request.get("comment")
-        comment = commentin.replace('\n', '<br>')
-        commentauthor = self.user.name
         commentid = int(p.key().id())
-
-        if comment and commentid:
-            c = Comment(parent = blog_key(), comment=comment, commentauthor=commentauthor, commentid = commentid)
-            c.put()
+        comment = self.request.get("comment")
+        formatted_comment = comment.replace('\n', '<br>')
+        all_comments = Comment.all().filter('commentid =', commentid).order('-created')
+        if self.user and comment: 
+            b = Comment(parent = blog_key(), comment = formatted_comment, commentauthor = p.author, commentid=commentid, all_comments = all_comments)
+            b.put()
+            #
             self.redirect("/")
         else:
-            error = "You have to enter text in the comment field!"
-            self.render("comment.html", p=p, subject=p.subject, content=p.content, error=error)
+            error = "To publish a blog post, you must be signed in and comment content is required in text area"
+            self.render("comment.html", p=p, error = error)
 
+class EditComment(Handler):
+	def get(self, comment_id):
+		key = db.Key.from_path('Comment', int(comment_id), parent = blog_key())
+		c = db.get(key)
 
+		if self.user.name == c.commentauthor:
+			self.render("edit_comment.html", c=c ,comment = c.comment)
+		else:
+			error = "You can only edit your own comment, and you have to be signed in to do that"
+			self.render("login.html", error = error)
 
+	def post(self, comment_id):
+		key = db.Key.from_path('Comment', int(comment_id), parent = blog_key())
+		c = db.get(key)
+
+		comment = self.request.get("comment")
+		# Obvisouly this is the author, author = self.user.name
+		if comment:
+			c.comment = comment
+			c.put()
+			comment_id = str(p.key().id())
+			self.redirect("/blog/comment/edit/%s" % comment_id)
+		else:
+			error = "To publish a comment, content is required"
+			self.render("/blog/comment/edit/%s" % comment_id)
 #########      ---- MAIN PAGE ----
 #    ---- PARTICULAR POST -----
 
@@ -461,4 +482,5 @@ app = webapp2.WSGIApplication([('/', BlogFront),
 								('/blog/edit/([0-9]+)', EditBlogEntry),
 								('/blog/delete/([0-9]+)', DeleteBlogEntry),
 								('/blog/like/([0-9]+)', Like),
-								('/blog/comment/([0-9]+)', Comments)], debug = True)
+								('/blog/comment/([0-9]+)', CommentHandler),
+								('/blog/comment/edit/([0-9]+)', EditComment)], debug = True)
